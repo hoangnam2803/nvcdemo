f9949934585506d6c08c13f9bc7a8a239478cae9
hoangnam2803 <yasuodarkwind@gmail.com>
2025-04-23 08:30:46 +0700

    first upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/PTIT2402/nvcdemo/',
+    'baseURL' => 'http://localhost/PTIT2402/nvcdemo/',
+    'assets' => '/PTIT2402/nvcdemo/assets/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'product',
+        'username' => 'root',
+        'password' => ''
+    )
+);
